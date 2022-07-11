9881e0be9807b7426a384ed0c7de5c44fcfeacc3
James Finch <LubricantJam@users.noreply.github.com>
2022-07-11 18:51:19 +0100

    Added on the hour checks.

--- refresh.sh
+++ refresh.sh
@@ -2,4 +2,14 @@
 
 docker-php-entrypoint apache2-foreground
 
-while true; do wget -O - https://kofi.karna.ge/webhook.php >/dev/null 2>&1; sleep 3600; done
+while true; do
+	DATE=`date +%Y%m%d`
+	HOUR=`date +%H`
+
+	while [ $HOUR -ne "00" ]; do
+        echo "Checking for expired roles...";
+		wget -O - https://kofi.karna.ge/webhook.php >/dev/null 2>&1
+		sleep 3600
+		HOUR=`date +%H`
+	done
+done
