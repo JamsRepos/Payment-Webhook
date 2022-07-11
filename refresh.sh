757a5db40c0704754b516f6102f29aaadfbb06f8
James Finch <LubricantJam@users.noreply.github.com>
2022-07-11 20:32:08 +0100

    Fixed the loop.

--- refresh.sh
+++ refresh.sh
@@ -8,10 +8,9 @@ while true; do
 	MINUTE=`date +%M`
 
 	# If the minute of the hour is 0, run the script
-	if [ "$MINUTE" = "00" ]; then
-		sleep 5
-        echo "Checking for expired roles...";
+	while [ $MINUTE -eq "00" ]; do
+		echo "Checking for expired roles...";
 		wget -O - https://kofi.karna.ge/webhook.php >/dev/null 2>&1
 		sleep 60
-	fi
+	done
 done
