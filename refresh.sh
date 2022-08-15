cfd94c73fbee62fba0b564b5b7886c13124248c2
James Finch <LubricantJam@users.noreply.github.com>
2022-08-15 11:50:07 +0100

    Updated domain.

--- refresh.sh
+++ refresh.sh
@@ -10,7 +10,7 @@ while true; do
 	# If the minute of the hour is 0, run the script
 	while [ $MINUTE -eq "00" ]; do
 		echo "Checking for expired roles...";
-		wget -O - https://kofi.karna.ge/webhook.php >/dev/null 2>&1
+		wget -O - https://pay.karna.ge/webhook.php >/dev/null 2>&1
 		# Run a second less to keep it a perfect loop
 		sleep 3599
 		MINUTE=`date +%M`
